504484ffdb4663ea49510613a5530d90dc9fa013
Chinh DT <chinhdtth2009021@fpt.edu.vn>
2021-07-26 11:59:08 +0700

    apartment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Http\Controllers\ApartmentsController;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(Apartmentseeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApartmentsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('/apartment',ApartmentsController::class,);
+Route::get('apartment/create',[ApartmentsController::class, 'create']);
+Route::post('apartment/create',[ApartmentsController::class, 'store']);
+Route::get('apartment/list',[ApartmentsController::class, 'index']);
+Route::get('apartment/edit/{id}',[ApartmentsController::class, 'update']);
+Route::post('apartment/edit/{id}',[ApartmentsController::class, 'save']);
+Route::delete('apartment/delete/{id}',[ApartmentsController::class, 'delete']);
